9e82ee94f03bd339e94c01a700d65d21d3baa576
yzhang3 <yzhang3@linkernetworks.com>
2016-07-03 11:24:50 -0400

    rm 3rd party package

--- README.md
+++ README.md
@@ -1,28 +1,10 @@
 # call-broswer
-call system default web broswer to open a link.
+Call system default web broswer to open a link.
 
 # Run
-
-check your go version by `go version`
-
-if go version < 1.5.0
-	* go get github.com/skratchdot/open-golang/open
-	* set GOPATH
-	* go build main.go
-	* run
-
-else if go version == 1.5.x
-	* go get github.com/Masterminds/glide
 	* copy this project under GOPATH
-	* glide init && glide install
-	* export GO15VENDOREXPERIMENT=1
 	* go build src/main.go
-
-else if go version >=1.6.0
-	* go get github.com/Masterminds/glide
-	* copy this project under GOPATH
-	* glide init && glide install
-	* go build src/main.go
+	* ./main
 
 # Related
 
--- src/main.go
+++ src/main.go
@@ -2,12 +2,41 @@ package main
 
 import (
 	"log"
-
-	"github.com/skratchdot/open-golang/open"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 )
 
 func main() {
-	if err := open.Run("https://github.com"); err != nil {
+	url := "https://github.com"
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "linux":
+		cmd = open_linux(url)
+	case "darwin":
+		cmd = open_darwin(url)
+	case "windows":
+		cmd = open_windows(url)
+	default:
+		log.Fatalf("Cannot open url: %s", "operating system not supported")
+	}
+
+	if err := cmd.Run(); err != nil {
 		log.Fatal("Open url error: %v", err)
 	}
+}
+
+func open_linux(url string) *exec.Cmd {
+	return exec.Command("xdg-open", url)
+}
+
+func open_darwin(url string) *exec.Cmd {
+	return exec.Command("open", url)
+}
+
+func open_windows(url string) *exec.Cmd {
+	cmd := "url.dll,FileProtocolHandler"
+	runDll32 := filepath.Join(os.Getenv("SYSTEMROOT"), "System32", "rundll32.exe")
+	return exec.Command(runDll32, cmd, url)
 }
